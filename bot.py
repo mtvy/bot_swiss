6ba8ad578aeef5c9d368b49f9dc0814db472e1e7
=='mtvy <=='mtvy@gmail.com>
2021-01-28 18:43:28 +0300

    classes_add

--- bot.py
+++ bot.py
@@ -2,7 +2,16 @@ from lib import *
 
 def openfileforRead(action=None, name_path=None):
     global account_settings
-    if action == 'r':
+    if action == 'set':
+        with open(path_acc_settings, 'r') as file_set:
+            if(file_set.readline() == ""): 
+                account_settings = {}
+            else:
+                file_set.close()
+                with open(path_acc_settings, 'r') as file_set:
+                    account_settings = json.load(file_set)
+        return account_settings
+    elif action == 'r':
         with open(path_acc_settings, 'r') as file_set:
             if(file_set.readline() == ""): 
                 account_settings = {}
---
+++ classes.py
@@ -0,0 +1,50 @@
+from lib import json
+import variables, path
+
+class Account:
+
+    def __init__(self, i, acc):
+        self.personal_data = acc[i]['personal data']
+        self.conversation = acc[i]['conversation']
+        self.feedback_st = acc[i]['feedback_st']
+        self.language = acc[i]['language']
+        self.oper_ids = acc[i]['oper_ids']
+        self.discount = acc[i]['discount']
+        self.login = acc[i]['login']
+        self.name = acc[i]['name']
+        self.tags = acc[i]['tags']
+        self.ref = acc[i]['ref']
+    
+    def __str__(self):
+        return str({
+            'login': self.login,
+            'name':  self.name,
+            'oper_ids':  self.oper_ids,
+            'conversation': self.conversation,
+            'discount':  self.discount,
+            'tags': self.tags,
+            'ref':  self.ref,
+            'personal data': self.personal_data,
+            'language': self.language,
+            'feedback_st': self.feedback_st,
+        })
+        
+def openfileforRead(action=None, name_path=None):
+    global account_settings
+    if action == 'set':
+        with open(path.path_acc_settings, 'r') as file_set:
+            if(file_set.readline() == ""): 
+                account_settings = {}
+            else:
+                file_set.close()
+                with open(path.path_acc_settings, 'r') as file_set:
+                    account_settings = json.load(file_set)
+        return account_settings
+        
+def read():
+    acc = openfileforRead('set')
+    box = [['i', Account(i,acc)] for i in acc]
+    for i in box:
+        print(i[1])
+if __name__ == '__main__':
+    read()
