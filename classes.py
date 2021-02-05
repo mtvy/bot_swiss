0dbc1386e67cbb39a267a5bd21778d2e99a3d197
=='mtvy <=='mtvy@gmail.com>
2021-02-06 00:57:28 +0300

    Initial commit

--- classes.py
+++ classes.py
@@ -1,15 +1,15 @@
-from lib import json
-import variables, path
+from lib import *
 
 class Account:
 
     def __init__(self, i, acc):
         self.personal_data = acc[i]['personal data']
         self.conversation = acc[i]['conversation']
-        self.feedback_st = acc[i]['feedback_st']
+        #self.feedback_st = acc[i]['feedback_st']
         self.language = acc[i]['language']
         self.oper_ids = acc[i]['oper_ids']
         self.discount = acc[i]['discount']
+        #self.timer = acc[i]['timer_conv']
         self.login = acc[i]['login']
         self.name = acc[i]['name']
         self.tags = acc[i]['tags']
@@ -26,18 +26,19 @@ class Account:
             'ref':  self.ref,
             'personal data': self.personal_data,
             'language': self.language,
-            'feedback_st': self.feedback_st,
+            #'feedback_st': self.feedback_st,
+            #'timer_conv': self.timer,
         })
         
 def openfileforRead(action=None, name_path=None):
     global account_settings
     if action == 'set':
-        with open(path.path_acc_settings, 'r') as file_set:
+        with open(path_acc_settings, 'r') as file_set:
             if(file_set.readline() == ""): 
                 account_settings = {}
             else:
                 file_set.close()
-                with open(path.path_acc_settings, 'r') as file_set:
+                with open(path_acc_settings, 'r') as file_set:
                     account_settings = json.load(file_set)
         return account_settings
         
